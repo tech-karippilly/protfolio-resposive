b349e4a015bb7d9455346e6d899413e244f7bfe0
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-10-22 18:32:41 +0530

    about section -mobile view added

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ const linkAction =()=>{
 }
 
 navLink.forEach(items=>items.addEventListener('click',linkAction))
+
+
+const blurHeader = ()=>{
+    const header = document.getElementById('header')
+    this.scrollY >=50 ?header.classList.add('blur-header')
+                    : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll',blurHeader)
